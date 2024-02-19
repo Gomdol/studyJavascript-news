36bba4ee4d08dea3e954adfb17809d7192a669c8
Gomdol <im.haru3@gmail.com>
2024-02-19 19:10:53 +0900

    누나API로 변경

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-const API_KEY = `1125cb34731a40b69604262079905fec`;
 let newsList = [];
 const getLatestNews = async () => {
     const url = new URL(
-        `https://newsapi.org/v2/top-headlines?country=us&pageSize=2&apiKey=${API_KEY}`
+        `https://pooh-news.netlify.app/top-headlines?q=${keyword}&country=kr&pageSize=${PAGE_SIZE}`
     );
     const response = await fetch(url);
     const data = await response.json();
