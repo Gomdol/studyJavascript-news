082b2ce4520ba7cdfd7391963af22c0af3e77e1e
Gomdol <im.haru3@gmail.com>
2024-02-22 20:52:23 +0900

    카테고리 적용

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 let newsList = [];
+const menus = document.querySelectorAll(".menus button")
+menus.forEach((menu) =>
+  menu.addEventListener("click", (event) => getNewsByCategory(event)));
+
 const getLatestNews = async () => {
-    const url = new URL(
-        `https://pooh-news.netlify.app/top-headlines?q=${keyword}&country=kr&pageSize=${PAGE_SIZE}`
-    );
-    const response = await fetch(url);
-    const data = await response.json();
-    newsList = data.articles;
-    render();
-    console.log("news:",newsList);
+  const url = new URL(
+    `https://pooh-news.netlify.app/top-headlines?country=kr`
+  );
+  const response = await fetch(url);
+  const data = await response.json();
+  newsList = data.articles;
+  render();
+  console.log("news:", newsList);
 };
+
+const getNewsByCategory = async (event) => {
+  const category = event.target.textContent.toLowerCase();
+  console.log("category:",category)
+  const url = new URL(`https://pooh-news.netlify.app/top-headlines?country=kr&category=${category}`)
+  const response = await fetch(url)
+  const data = await response.json()
+  console.log("DDD :", data)
+  newsList = data.articles;
+  render();
+}
 
 getLatestNews();
 
 const render = () => {
-    const newsHTML = newsList.map(news => `<article class="row news">
+  const newsHTML = newsList.map(news => `<article class="row news">
     <picture class="col-lg-4">
       <img
         class="news-image-size"
@@ -29,7 +44,7 @@ const render = () => {
 
 
 
-    document.getElementById("news-board").innerHTML = newsHTML;
+  document.getElementById("news-board").innerHTML = newsHTML;
 }
 
 
@@ -47,16 +62,16 @@ const render = () => {
 
 
 function openSearch() {
-    // 인풋창과 버튼 요소를 변수에 할당
-    let searchInput = document.querySelector('.search-input');
-    let searchButton = document.getElementById('button-addon2');
+  // 인풋창과 버튼 요소를 변수에 할당
+  let searchInput = document.querySelector('.search-input');
+  let searchButton = document.getElementById('button-addon2');
 
-    // 인풋창의 현재 display 상태를 확인하고 토글
-    if (searchInput.style.display === 'block') {
-        searchInput.style.display = 'none';
-        searchButton.style.display = 'none';
-    } else {
-        searchInput.style.display = 'block';
-        searchButton.style.display = 'block';
-    }
+  // 인풋창의 현재 display 상태를 확인하고 토글
+  if (searchInput.style.display === 'block') {
+    searchInput.style.display = 'none';
+    searchButton.style.display = 'none';
+  } else {
+    searchInput.style.display = 'block';
+    searchButton.style.display = 'block';
+  }
 }
