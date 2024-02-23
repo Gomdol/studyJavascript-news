e080870f039fd1ccc4bd72c38c245110e2e13b54
Gomdol <im.haru3@gmail.com>
2024-02-24 00:11:07 +0900

    make pagination

--- script.js
+++ script.js
@@ -5,17 +5,29 @@ menus.forEach((menu) =>
 
 let url = new URL(`https://pooh-news.netlify.app/top-headlines?country=kr`);
 
+let totalResults = 0
+let page = 1
+const pageSize = 10
+const groupSize = 5
+
 const getNews = async () => {
 
   try {
+    url.searchParams.set("page",page);
+    url.searchParams.set("pageSize",pageSize);
+
     const response = await fetch(url)
+
     const data = await response.json()
+    console.log("data:",data)
     if (response.status === 200) {
       if (data.articles.length === 0) {
         throw new Error("검색 결과가 없습니다.");
       }
       newsList = data.articles;
+      totalResults = data.totalResults;
       render();
+      paginationRender();
     } else {
       throw new Error(data.message)
     }
@@ -81,4 +93,36 @@ function openSearch() {
     searchInput.style.display = 'block';
     searchButton.style.display = 'block';
   }
+}
+
+const paginationRender = () => {
+  //totalResult
+  //page
+  //pageSize
+  //groupSize
+  //totalPages
+  const totalPages = Math.ceil(totalResults / pageSize)
+  //pageGroup 몇 번째 페이지 그룹에 속해있는지?
+  const pageGroup = Math.ceil(page / groupSize) 
+  //lastPage 그 그룹의 마지막 페이지 번호
+  let lastPage = pageGroup * groupSize
+  if(lastPage>totalPages){
+    lastPage = totalPages
+  }
+  //firstPage 그 그룹의 첫번째 페이지 번호
+  const firstPage = lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1);
+
+  let paginationHTML = ``
+
+  for(let i=firstPage;i<=lastPage;i++){
+    paginationHTML+=`<li class="page-item ${i === page?"active":""}" onclick="moveToPage(${i})"><a class="page-link" >${i}</a></li>`
+  }
+
+  document.querySelector(".pagination").innerHTML=paginationHTML
+}
+
+const moveToPage = (pageNum) =>{
+  console.log("moveToPage",pageNum)
+  page = pageNum
+  getNews();
 }
